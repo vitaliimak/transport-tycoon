8a24d01740567dedc85da84900f35277250e4a83
Vitalii Makarchenkov <vitalii.mak@gmail.com>
2021-07-29 19:56:43 +0300

    Add server skeleton

---
+++ server/src/main/java/com/github/vitaliimak/transporttycoon/TransporttycoonApplication.java
@@ -0,0 +1,13 @@
+package com.github.vitaliimak.transporttycoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransporttycoonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransporttycoonApplication.class, args);
+    }
+
+}
---
+++ server/src/test/java/com/github/vitaliimak/transporttycoon/TransporttycoonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.vitaliimak.transporttycoon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransporttycoonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
