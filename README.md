27ca8bb0ee292fbb7f0243a67d719ceb2094f036
Vitalii Makarchenkov <vitalii.mak@gmail.com>
2021-09-16 22:24:32 +0300

    Add forms for login and registration

--- README.md
+++ README.md
@@ -15,3 +15,11 @@
 docker exec -it transport-tycoon-db /bin/bash
 psql -U app_user transport_tycoon -W
 ```
+
+
+## Client
+## Development server
+
+1. Go to `client` folder
+2. Run `npm install` to install dependencies
+3. Run `npm start` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,37 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NbAuthComponent, NbLoginComponent, NbRegisterComponent } from '@nebular/auth';
+import { HomeModule } from './pages/home/home.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/auth/login',
+    pathMatch: 'full',
+  },
+  {
+    path: 'auth',
+    component: NbAuthComponent,
+    children: [
+      {
+        path: '',
+        component: NbLoginComponent,
+      },
+      {
+        path: 'login',
+        component: NbLoginComponent,
+      },
+      {
+        path: 'register',
+        component: NbRegisterComponent,
+      },
+    ],
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule),
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
